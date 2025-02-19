db52e9e30cd31081b2ff329f3bea9031cd89878b
simpsaad <155128768+simpsaad@users.noreply.github.com>
2025-02-19 00:18:21 -0800

    Add files via upload

---
+++ multiple-choice-quiz-master/assets/js/game.js
@@ -0,0 +1,124 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+  {
+    question: "Which level of biodiversity refers to the variety of genes within a species?",
+    choice1: "ecosystem diversity",
+    choice2: "Species diversity",
+    choice3: "Genetic diversity",
+    choice4: "Habitat diversity",
+    answer: 3
+  },
+  {
+    question: "Which of the following is a major threat to biodiversity?",
+    choice1: "Sustainable agriculture",
+    choice2: "Habitat destruction",
+    choice3: "Ecotourism",
+    choice4: "Wildlife conservation",
+    answer: 2
+  },
+  {
+    question: "Why is species diversity Important for ecosystem?",
+    choice1: "It slow down ecological processes",
+    choice2: "It helps in maintain ecosystem stability and resilience",
+    choice3: "it reduces the number of food sources",
+    choice4: "It prevents natural selection from occouring",
+    answer: 2
+  }
+];
+
+//CONSTANTS
+const INCORRECT_TAX = 10;
+const MAX_QUESTIONS = 3;
+
+// Start Game & Timer
+startGame = () => {
+  questionCounter = 0;
+  score = 100;
+  availableQuesions = [...questions];
+  getNewQuestion();
+
+  // Timer
+  setInterval(function () {
+    score--;
+    scoreText.innerText = score;
+
+    if (score === 0) {
+      localStorage.setItem("mostRecentScore", score);
+
+      //go to the end page
+      return window.location.assign("../../assets/html/end.html");
+    }
+  }, 1000);
+};
+
+// Display Next Random Question and Answers
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+
+    //go to the end page
+    return window.location.assign("../html/end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  // Get Answers
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+//Get User's Choice
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "incorrect") {
+      decrementScore(INCORRECT_TAX);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+//Penalty for wrong choice
+decrementScore = num => {
+  score -= num;
+  scoreText.innerText = score;
+};
+
+
+startGame();
